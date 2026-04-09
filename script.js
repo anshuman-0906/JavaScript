e40d7a4980ff87e7cb7e2c8023205c58c20a90e1
anshuman-0906 <anshuman.mishra0906@gmail.com>
2026-04-09 20:03:32 +0530

    learning more concepts

---
+++ script.js
@@ -0,0 +1,13 @@
+let companies=["Microsoft","Amazon","google","TCS","KickDrum","Azure"];
+companies.push("Spindex");
+
+let a=10,b=15;
+// function sum(m,k){
+//     return m+k;
+// }
+let s=(g,k)=>{return(g+k)}
+
+// companies.forEach((d,h)=>{console.log(d,"do me a favour and tell everyone that i am leaving",h)});
+
+let arr=[1,2,3,4,5,6,7,8,9];
+arr.forEach((i)=>{console.log(i*i)});
