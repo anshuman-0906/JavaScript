20f497a93c60f4b2c31e039542e606f2a52776a6
anshuman-0906 <anshuman.mishra0906@gmail.com>
2026-04-09 22:48:52 +0530

    for(in vs of)

--- script.js
+++ script.js
@@ -9,5 +9,14 @@ let s=(g,k)=>{return(g+k)}
 
 // companies.forEach((d,h)=>{console.log(d,"do me a favour and tell everyone that i am leaving",h)});
 
-let arr=[1,2,3,4,5,6,7,8,9];
-arr.forEach((i)=>{console.log(i*i)});
+ //let arr=[1,2,3,4,5,6,7,8,9];
+// arr.forEach((i)=>{console.log(i*i)});
+
+str="anshuman";
+let arr=[111,21,32,46,45,64,76,78,99];
+for(let key in arr){
+    console.log(key);
+}
+for(let i of arr){
+    console.log(i);
+}
