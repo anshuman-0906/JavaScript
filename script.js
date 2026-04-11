43c36fba8eeb104cca686ac95f00ee0c85f98f26
anshuman-0906 <anshuman.mishra0906@gmail.com>
2026-04-11 23:40:03 +0530

    lrn

--- script.js
+++ script.js
@@ -12,17 +12,23 @@ let s=(g,k)=>{return(g+k)}
  //let arr=[1,2,3,4,5,6,7,8,9];
 // arr.forEach((i)=>{console.log(i*i)});
 
-str="anshuman";
-let arr=[111,21,32,46,45,64,76,78,99];
-for(let key in arr){
-    console.log(key);
-}
-for(let i of arr){
-    console.log(i);
-}
+// str="anshuman";
+// let arr=[111,21,32,46,45,64,76,78,99];
+// for(let key in arr){
+//     console.log(key);
+// }
+// for(let i of arr){
+//     console.log(i);
+// }
 let heading=document.getElementById("heading1");
 console.log(heading);
 console.dir(heading);
 
 let qry=document.querySelector("#name19");
 console.dir(qry);
+
+let headt=document.querySelectorAll("#ex1");
+//let headh=document.querySelector("#a");
+console.log(headt);
+console.dir(headt);
+
